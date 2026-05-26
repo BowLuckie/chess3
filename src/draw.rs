7e4b604c5e9d70fe6efa71b7637996efbf9562a3
Bowluckie <bowluckie@gmail.com>
2026-05-26 20:00:42 +1000

    added raylib board drawing and improved pawn movement logic

---
+++ src/draw.rs
@@ -0,0 +1,61 @@
+use raylib::prelude::*;
+use crate::{Board, get_lexrep};
+
+const TILE_SIZE: i32 = 80;
+
+pub fn main() {
+    let (mut r1, thread) = raylib::init()
+        .size(TILE_SIZE * 8, TILE_SIZE * 8)
+        .title("chess3")
+        .build();
+
+    let board = Board::new();
+
+    while !r1.window_should_close() {
+        let mut d = r1.begin_drawing(&thread);
+
+        d.clear_background(Color::DARKGRAY);
+
+       draw_board(&mut d); 
+       draw_pieces(&mut d, &board);
+    }
+}
+
+fn draw_board(d: &mut RaylibDrawHandle) {
+    let light = Color::new(240, 217, 181, 255); 
+    let dark  = Color::new(181, 136, 99, 255); 
+    for row in 0..8 {
+        for col in 0..8 {
+            let colour = if (row + col) % 2 == 0 {
+                light
+            } else {
+                dark
+            };
+
+            d.draw_rectangle(col as i32 * TILE_SIZE, row as i32 * TILE_SIZE, TILE_SIZE, TILE_SIZE, colour);
+        }
+    }
+}
+
+fn draw_pieces(d: &mut RaylibDrawHandle, board: &Board) {
+    for row in 0..8 {
+        for col in 0..8 {
+            let piece = board.get_piece(row, col);
+
+            if let Some(p) = *piece {
+                let opt = Some(p);
+                let symbol = get_lexrep(&opt);
+
+                let font_size = 30;
+
+                let text_width = d.measure_text(symbol, font_size);
+                let text_height = font_size;
+
+                let x = col as i32 * TILE_SIZE + (TILE_SIZE - text_width) / 2;
+                let y = row as i32 * TILE_SIZE + (TILE_SIZE - text_height) / 2;
+
+                d.draw_text(symbol, x, y, font_size, Color::BLACK);
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::fmt;
+use std::{fmt, vec};
+use crate::Colour::{Black, White};
 
-pub type Coordinate = (u8, u8);
+pub type Coordinate = (i8, i8);
 
-#[derive(Debug, Clone, Copy)]
+mod draw;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Colour {
     White,
     Black,
@@ -19,10 +22,12 @@ pub enum PieceKind {
 pub struct Piece {
     kind: PieceKind,
     colour: Colour,
+    has_moved: bool,
 }
 
 
 #[derive(Debug, Clone, Copy)]
+#[allow(dead_code)]
 pub struct Board {
     squares: [[Option<Piece>; 8]; 8],
     to_move: Colour,
@@ -33,6 +38,7 @@ pub enum BoardMode {
     Standard,
 }
 
+#[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 pub struct Move {
     from: Coordinate,
@@ -40,11 +46,20 @@ pub struct Move {
 }
 
 impl Move {
-    fn new_move(from: Coordinate, to: Coordinate) -> Move {
+    fn new(from: Coordinate, to: Coordinate) -> Move {
         Move {from: from, to: to}
     }
 }
 
+fn get_lexrep(piece: &Option<Piece>) -> &str {
+    match piece {
+        Some(piece) => match (piece.kind, piece.colour) {
+            (PieceKind::Pawn, Colour::White) => "[P]",
+            (PieceKind::Pawn, Colour::Black) => "[p]",
+        },
+        None => "[ ]",
+    }
+}
 
 impl fmt::Display for Board {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -52,13 +67,7 @@ impl fmt::Display for Board {
             write!(f, "{} ", row)?;
 
             for col in 0..8 {
-                let symbol = match self.squares[row][col] {
-                    Some(piece) => match (piece.kind, piece.colour) {
-                        (PieceKind::Pawn, Colour::White) => 'P',
-                        (PieceKind::Pawn, Colour::Black) => 'p',
-                    },
-                    None => ' ',
-                };
+                let symbol = get_lexrep(self.get_piece(row, col));
 
                 write!(f, "[{}]", symbol)?;
             }
@@ -78,6 +87,7 @@ impl Board {
             squares[1][col] = Some(Piece {
                 kind: PieceKind::Pawn,
                 colour: Colour::Black,
+                has_moved: false,
             });
         }
 
@@ -85,40 +95,64 @@ impl Board {
             squares[6][col] = Some(Piece {
                 kind: PieceKind::Pawn,
                 colour: Colour::White,
+                has_moved: false,
             });
         }
 
         Self { squares: squares, to_move: Colour::White }
     }
 
-    fn dispatch(&self, p: Piece, row: u8, col: u8) -> Vec<Move> {
+    fn dispatch(&self, p: Piece, row: i8, col: i8) -> Vec<Move> {
         use PieceKind::*;
         match p.kind {
             Pawn => self.pawn_moves(p, row, col) 
         }
     }
 
-    pub fn get_piece(&self, row: u8, col: u8) -> &Option<Piece> {
+    pub fn get_piece(&self, row: i8, col: i8) -> &Option<Piece> {
+        if row > 7 || col > 7 {
+            panic!("row or col exceeds 7 {} {}", row, col)
+        }
         &self.squares[row as usize][col as usize]
     }
 
-    pub fn get_moves(&self, row: u8, col: u8) -> Vec<Move> {
+    pub fn get_moves(&self, row: i8, col: i8) -> Vec<Move> {
        match *self.get_piece(row, col) {
             Some(p) => self.dispatch(p, row, col),
             None => vec![],
        }
     }
 
-    pub fn pawn_moves(&self, p: Piece, row: u8, col: u8) -> Vec<Move> {
-        use Colour::*;
-        let dir: i8 = match p.colour {
+    fn pawn_moves(&self, p: Piece, row: i8, col: i8) -> Vec<Move> {
+        let dir = match p.colour {
             Black => 1,
             White => -1,
         };
 
-        let target_row = (row as i8 + dir) as u8;
+        let mut moves = Vec::new();
+        let origin = (row, col);
+        
+        
+        let new_row: i8 = row + dir;
 
-        vec![Move::new_move((row, col), (target_row, col))]
+        if (0..8).contains(&new_row) && self.get_piece(new_row, col).is_none() {
+            moves.push(Move::new(origin, (new_row, col)));
+            if !p.has_moved && self.get_piece(new_row + dir, col).is_none() {
+               moves.push(Move::new(origin, (new_row + dir, col))); 
+            }
+        }
+
+        for dc in [-1, 1] {
+            let new_col = col + dc;
+            if (0..8).contains(&new_col) && (0..8).contains(&new_row) {
+                let target = self.get_piece(new_row, new_col);
+                if target.is_some() && target.unwrap().colour != p.colour {
+                    moves.push(Move::new((new_row, col), (new_row, new_col)));   
+                }
+            }
+        }
+
+        return moves;
     }
 }
 
@@ -126,6 +160,9 @@ fn main() {
     let board = Board::new();
     println!("{}", board);
 
+
     println!("{:?}", board.get_piece(6, 0));
-    println!("{:?}", board.get_moves(6, 0))
+    println!("{:?}", board.get_moves(6, 0));
+
+    draw::main();
 }
